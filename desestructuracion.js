216ce1b894d25f6ab90850ed1b8034f07f1c28f9
AdalObregon <adalobregoon@gmail.com>
2022-03-26 16:36:51 -0600

    26-03-22

--- desestructuracion.js
+++ desestructuracion.js
@@ -75,3 +75,118 @@ const {
 
 console.log(minima);
 console.log(maxima);
+
+/*
+    Sintaxis de desestructuracion: Arreglos.
+
+    La sintaxis de desestructuracion tambien sirve para asignar variables a arreglos.
+*/
+
+var a;
+var b;
+
+[a, b] = [1, 2];
+console.log(a);
+console.log(b);
+
+var c;
+
+[a, b, c] = [1, 2, 3];
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a;
+var b;
+
+[a, b] = [1, 2, 3, 4, 5, 6];
+
+console.log(a);
+console.log(b);
+
+//asignar el valor 5 para saltarnos la posicion ponemos tres ','
+
+var a;
+var b;
+var c;
+
+[a, b, , , c] = [1, 2, 3, 4, 5, 6];
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Cambiar o intercambiar valores de dos Variables
+
+var a = 8;
+var b = 6;
+
+[b, a] = [a, b];
+console.log(a);
+console.log(b);
+
+/*
+    Desestructuracion para rest
+*/
+var a;
+var b;
+[a, b, ...arr] = [1, 2, 3, 4, 5, 6, 7];
+console.log(a);
+console.log(b);
+console.log(arr);
+
+//remover los primeros 3 elementos de un arreglo
+
+const arregloInicial = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function removerTresPrimerosElementos(arr) {
+  const [, , , ...nuevoArreglo] = arr;
+  return nuevoArreglo;
+}
+const arregloFinal = removerTresPrimerosElementos(arregloInicial);
+console.log(arregloFinal);
+
+//Pasar objeto como argumento
+
+var nuevoPerfilCliente = {
+  nombre: "Jane Doe",
+  edad: 24,
+  nacionalidad: "Española",
+  ubicacion: "España",
+};
+
+const actualizarPerfil1 = (informacionDePerfil) => {
+  const { nombre, edad, nacionalidad, ubicacion } = informacionDePerfil;
+  console.log(nombre);
+  console.log(edad);
+  console.log(nacionalidad);
+  console.log(ubicacion);
+};
+
+//pasando como argumento
+const actualizarPerfil = ({ nombre, edad, nacionalidad, ubicacion }) => {
+  console.log(nombre);
+  console.log(edad);
+  console.log(nacionalidad);
+  console.log(ubicacion);
+};
+
+actualizarPerfil(nuevoPerfilCliente);
+
+//Otro ejemplo
+
+const estadisticas = {
+  max: 56.78,
+  desviacionEstandar: 4.34,
+  mediana: 34.54,
+  moda: 23.87,
+  min: -0.75,
+  promedio: 35.85,
+};
+
+const puntoMedio = (est) => (est.max + est.min) / 2.0;
+
+// con parametro
+const mitad = ({max,min}) => (max + min) / 2.0;
+console.log(mitad(estadisticas));
+console.log(puntoMedio(estadisticas));
